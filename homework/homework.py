2f3212469a475d25d8c830bad6192574cf4105ef
DanielCaroD <dcarod@unal.edu.co>
2025-01-16 20:37:39 -0500

    .

--- homework/homework.py
+++ homework/homework.py
@@ -50,8 +50,45 @@ def clean_campaign_data():
 
     """
 
-    return
+    import glob
+    import pandas as pd
+    import os
 
+    zip_files = glob.glob("files/input/**/*.zip", recursive=True)
+
+    columns = ["client_id", "age", "job", "marital", "education", "credit_default", "mortgage", "number_contacts", "contact_duration",
+               "previous_campaign_contacts", "previous_outcome", "campaign_outcome", "last_contact_date", "cons_price_idx", "euribor_three_months"]
+    
+    client_data = []
+    campaign_data = []
+    economics_data = []
+
+    for zip_file in zip_files:
+        df = pd.read_csv(zip_file)
+        df["job"] = df["job"].str.replace(".", "").str.replace("-", "_")
+        df["education"] = df["education"].str.replace(".", "_")
+        df["education"] = df["education"].apply(lambda x: pd.NA if x == "unknown" else x)
+        df["credit_default"] = df["credit_default"].apply(lambda x: 1 if x == "yes" else 0)
+        df["mortgage"] = df["mortgage"].apply(lambda x: 1 if x == "yes" else 0)
+        df["previous_outcome"] = df["previous_outcome"].apply(lambda x: 1 if x == "success" else 0)
+        df["campaign_outcome"] = df["campaign_outcome"].apply(lambda x: 1 if x == "yes" else 0)
+        df["last_contact_date"] = pd.to_datetime(df["day"].astype(str) + '-' + df["month"] + "-2022", format="%d-%b-%Y")
+        client_data.extend(df[columns[:7]].values.tolist())
+        campaign_data.extend(df[columns[0:1]].join(df[columns[7:13]]).values.tolist())
+        economics_data.extend(df[columns[0:1]].join(df[columns[13:]]).values.tolist())
+
+    client_pd = pd.DataFrame(client_data,columns=columns[:7])
+    campaign_pd = pd.DataFrame(campaign_data,columns=[columns[0]] + columns[7:13])
+    economics_pd = pd.DataFrame(economics_data,columns=[columns[0]] + columns[13:])
+    
+    route = "files/output"
+    os.makedirs(route, exist_ok=True)
+
+    client_pd.to_csv(os.path.join(route, "client.csv"), index=False)
+    campaign_pd.to_csv(os.path.join(route, "campaign.csv"), index=False)
+    economics_pd.to_csv(os.path.join(route, "economics.csv"), index=False)
+
+    return
 
 if __name__ == "__main__":
     clean_campaign_data()
